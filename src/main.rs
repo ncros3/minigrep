64c1ee83dc24a699a2b33dda2feca5145596ede8
ncros <ncros@macbook-pro-de-ncros.home>
2021-02-09 21:35:38 +0100

    add a constructor for Config struct

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let grepConfig: Config = parse_config(&args);
+    let grepConfig= Config::new(&args);
 }
 
 struct Config {
@@ -12,9 +12,11 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    Config {
-        query: args[1].clone(),
-        filename: args[2].clone(),
+impl Config {
+    fn new(args: &[String]) -> Config {
+        Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        }
     }
 }
