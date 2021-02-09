8de03e5fb5be12a69c00d24823d1dc61b1bedac7
ncros <ncros@macbook-pro-de-ncros.home>
2021-02-09 07:39:35 +0100

    refactoring pour inclure une structure

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let grepConfig: Config = parse_config(&args);
+}
 
-    println!("In file {}", filename);
+fn parse_config(args: &[String]) -> Config {
+    let returnConfig = Config {
+        query: args[1].clone(),
+        filename: args[2].clone(),
+    };
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+    returnConfig
 }
