fa5c947fdb0793764b330b17509172924b54a28d
ncros <ncros@macbook-pro-de-ncros.home>
2021-02-09 17:42:27 +0100

    directly return Config structure

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 use std::env;
 use std::fs;
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let grepConfig: Config = parse_config(&args);
 }
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
 fn parse_config(args: &[String]) -> Config {
-    let returnConfig = Config {
+    Config {
         query: args[1].clone(),
         filename: args[2].clone(),
-    };
-
-    returnConfig
+    }
 }
