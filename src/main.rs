4416a9b099c8f8ce33d3e8ea0fd6854113190055
ncros <ncros@macbook-pro-de-ncros.home>
2021-02-10 13:13:31 +0100

    add a test for library

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,13 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let grepConfig= Config::new(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments: {}", err);
+    let grep_config= Config::new(&args).unwrap_or_else(|err| {
+        println!("{}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", grepConfig.query);
-    println!("In file {}", grepConfig.filename);
-
-    if let Err(e) = minigrep::run(grepConfig) {
+    if let Err(e) = minigrep::run(grep_config) {
         println!("Application error : {}", e);
-
         process::exit(1);
     }
 }
